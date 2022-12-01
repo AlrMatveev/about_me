45730c19b9ffbdaf392d6d8e996aacf284286466
Alexandr Matveev <kareroyal@gmail.com>
2022-12-02 00:05:21 +0300

    loader

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,9 +4,14 @@ import { useTransition, animated, config } from "react-spring";
 import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "./Menu";
+import { useAppSelector } from "../hooks/redux";
+import { Box } from "@mui/material";
+import Loader from "./Loader";
 
 const Nav: FC = () => {
   const [menu, setMenu] = useState(false);
+  const load = useAppSelector((state) => state.pageReducer.status);
+  console.log(load);
   const transitions = useTransition(menu, {
     from: { opacity: 0, zIndex: 5000 },
     enter: { opacity: 1 },
@@ -23,6 +28,15 @@ const Nav: FC = () => {
     setMenu(false);
   };
 
+  const transitions2 = useTransition(load === "loading", {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    reverse: load === "loading",
+    delay: 200,
+    config: config.molasses,
+  });
+
   return (
     <div
       style={{
@@ -32,7 +46,12 @@ const Nav: FC = () => {
         margin: "10px",
       }}
     >
-      <IconButton size="large" color="primary" onClick={handleOpen}>
+      <IconButton
+        size="large"
+        color="primary"
+        sx={{ zIndex: "100" }}
+        onClick={handleOpen}
+      >
         <MenuIcon fontSize="inherit" />
       </IconButton>
       {transitions(
@@ -40,6 +59,14 @@ const Nav: FC = () => {
           item && (
             <animated.div style={styles}>
               <Menu handleClick={handleClose} />
+            </animated.div>
+          )
+      )}
+      {transitions2(
+        (styles, item) =>
+          item && (
+            <animated.div style={styles}>
+              <Loader />
             </animated.div>
           )
       )}
